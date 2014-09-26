ef5d971e4c6bceda37775ef07aeaa127e028099a
Addy Osmani <addyosmani@gmail.com>
2014-09-26 17:45:46 +0100

    Drop unnecessary checks.

--- fixmyjs.js
+++ fixmyjs.js
@@ -9,16 +9,6 @@ stdin(function (data) {
 		var js = fixmyjs.fix(data, opts);
 		process.stdout.write(js);
 	} catch (err) {
-		if (/Unclosed block/.test(err.message)) {
-			console.error('Couldn\'t find any valid JavaScript to fix.');
-			return;
-		}
-
-		if (err.name === 'TypeError') {
-			console.error('Invalid JS');
-			return
-		}
-
 		throw err;
 	}
 });

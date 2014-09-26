f5748a6592a7320377a4d4899b6859c81c7cc128
Addy Osmani <addyosmani@gmail.com>
2014-09-26 17:46:21 +0100

    Clean-up test some more.

--- test.js
+++ test.js
@@ -1,14 +1,10 @@
 // Array literal test
-var a = Array()
-var ab = []
+var a = Array();
+var ab = [];
 
 // Array constructions
 var b = new Array();
 var bb = [];
-
-// Curly braces for statements
-for (;;) x;
-for (;;) {\n  x;\n}
 
 // Literals
 var c = Object();

8ff360136cce88e8e51967c917c898ca16b2a761
Addy Osmani <addyosmani@gmail.com>
2014-09-26 18:44:18 +0100

    Tweak up test.js

--- test.js
+++ test.js
@@ -1,3 +1,9 @@
+// This code has issues
+'use strict';
+// Missing bits
+var q = Array()
+var t = new Object()
+
 // Array literal test
 var a = Array();
 var ab = [];
@@ -9,3 +15,14 @@ var bb = [];
 // Literals
 var c = Object();
 var cb = [];
+
+// Number constructions
+var d = new Number(3)
+
+// JSON constructions
+var e = new JSON({});
+
+// Leading decimals
+var g = .25;
+
+console.log(q,t,a,ab,b,bb,c,cb,d,e,g);
